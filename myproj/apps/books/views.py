bb3c3cd23ddff11e03e07680ef9be5b9c99fb3d9
honeda <50893852+honeda@users.noreply.github.com>
2023-04-25 21:14:37 +0900

    feat: django project

---
+++ myproj/apps/books/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.all()
+    context = {
+        "title": "List Books",
+        "books": books,
+    }
+    template = loader.get_template("books/list_books.html")
+    return HttpResponse(template.render(context, request))
